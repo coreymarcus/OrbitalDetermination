77e7484b2f2e246193df3114a9ee8b616f70e929
coreymarcus <corey.marcus@gmail.com>
2021-01-31 19:28:13 -0600

    making good progress with external libraries

--- src/HW1/main.cc
+++ src/HW1/main.cc
@@ -22,6 +22,25 @@ Eigen::VectorXf AnalyticOscill(float A, float ratio, Eigen::VectorXf t, float ph
 	return  y;
 }
 
+//quickly convert from eigen vector to std:vector
+std::vector<float> EigenVec2Std(Eigen::VectorXf vec1){
+	
+	//locals
+	int n = vec1.size();
+	std::vector<float> vec2(n,0);
+
+	//loop
+	for (int ii = 0; ii < n; ++ii){
+		vec2[ii] = vec1(ii);
+	}
+
+	//output
+	return vec2;
+}
+
+//namespaces, don't ever use name space, simply call them aliases
+namespace plt = matplotlibcpp;
+
 int main() {
 	
 	//initialize variables
@@ -33,6 +52,18 @@ int main() {
 	//call analytic function
 	Eigen::VectorXf y_analyt = AnalyticOscill(A, ratio, t, phi);
 
-	std::cout << y_analyt << "\n";
+
+	//plot results
+	std::vector<float> t_std = EigenVec2Std(t);
+	std::vector<float> y_analyt_std = EigenVec2Std(y_analyt);
+	std::vector<float> y_analyt_std2 = EigenVec2Std(2*y_analyt);
+
+	plt::named_plot("Analytic",t_std, y_analyt_std);
+	plt::named_plot("test2",t_std, y_analyt_std2);
+	plt::title("Homework 1 - Plot 1");
+	plt::xlabel("Time (s)");
+	plt::ylabel("System Response");
+	plt::legend();
+    plt::show();
 
 }
