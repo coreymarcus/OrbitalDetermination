13d324c2b7b6da29f11af098fff6e4df326d7f99
coreymarcus <corey.marcus@gmail.com>
2021-03-08 17:30:12 -0600

    started work on HW3

---
+++ src/HW3/VehicleState.h
@@ -0,0 +1,98 @@
+#ifndef OD_SRC_HW1_VEHICLESTATE_H_
+#define OD_SRC_HW1_VEHICLESTATE_H_
+
+#include <Eigen/Dense>
+#include <boost/numeric/odeint.hpp> //integrator
+#include <vector> // basic cpp vectors
+
+namespace VehicleState{
+
+	// type definitions for the propagator
+	namespace ode = boost::numeric::odeint;
+	typedef std::vector< double > state_type;
+	typedef ode::runge_kutta_dopri5< state_type > error_stepper_type;
+	typedef ode::controlled_runge_kutta< error_stepper_type > controlled_stepper_type;
+
+	//class to store and propagate the state of a vehicle
+	class Propagator{
+
+	public:
+
+		//parameters
+		bool useJ2_;
+		bool usedrag_;
+
+		//vehicle position and velocity (ECI) [km] and [km/sec]
+		Eigen::Vector3d pos_;
+		Eigen::Vector3d vel_;
+
+		//orbital elements
+		double a_; //semi-major axis
+		double e_; //eccentricity
+		double i_; //inclination
+		double ascend_; //longitude of ascending node
+		double periap_; //argument of periapsis
+		double nu_; //true anomaly
+		double P_; //orbital period
+		double T_p_; // time of perigee passage 
+
+		//physics constants
+		double mu_; //gravity constant [km^3/sec]
+		double J2_; //higher order gravity parameters
+		double Rearth_; //radius of the earth [km]
+		double earthrotationspeed_; //rotation [rad/sec]
+		double C_D_; // coefficient of drag
+		double A_; // effective area, m^2
+		double m_; // vehicle mass, kg
+		double rho_0_; // standard air density, kg/m^3
+		double r0_; // param for air density calc, km
+		double H_; //param for air density calc, km
+
+		//time
+		double t_;
+
+		//ode integrator elements
+		double dt_var_; //initial guess for a variable step size
+		double reltol_; //relative tolerance
+		double abstol_; //absolute tolerance		
+
+		// Constructor
+		Propagator();
+
+		//Use orbital elements to update the state vector
+		void OE2State();
+
+		//Use state vector to update orbital elements
+		void State2OE();
+
+		//call the numerical propagator from the current time, t_, to t_ + dt
+		void Propagate(double dt);
+
+		//accessor for the acceleration vector
+		Eigen::Vector3d GetAccelVector();
+
+		//accessor for the angular momentum vector
+		Eigen::Vector3d GetAngMomVector();
+
+		//accessor for specific kinetic energy
+		double GetKEsp();
+
+		//accessor for specific potential energy
+		double GetPEsp();
+
+		// () operator function reserved for orbit propagation with odeint
+		void operator()( const state_type &x , state_type &dxdt , const double /* t */ );
+
+	private:
+
+
+
+
+	}; // class Propagator
+
+
+
+} // namespace VehicleState
+
+
+#endif
---
+++ src/HW3/main.cc
@@ -0,0 +1,84 @@
+#include <iostream> //outputs
+#include <boost/numeric/odeint.hpp> //integrator
+#include <math.h> // trig functions
+#include <Eigen/Dense> // vectors
+#include "VehicleState.h" // headerfile containing propagator class
+#include "Util.h" // utility functions
+#include <iomanip>      // std::setprecision
+#include <unsupported/Eigen/MatrixFunctions>
+
+/* The type of container used to hold the state vector */
+typedef std::vector< double > state_type;
+
+//namespaces, don't ever use name space, simply call them aliases
+// namespace mat = matlab::engine;
+
+int main() {
+
+	//form a propagator object
+	VehicleState::Propagator propobj;
+
+	//set physics constants
+	propobj.mu_ = 1; // dimensionless
+
+	//parameters
+	propobj.useJ2_ = false;
+	propobj.usedrag_ = false;
+
+	//set objects position and velocity
+	Eigen::Vector3d pos0;
+	pos0[0] = 1.0;
+	pos0[1] = 0.0;
+	pos0[2] = 0.0;
+
+	Eigen::Vector3d vel0;
+	vel0[0] = 0.0;
+	vel0[1] = 1.0;
+	vel0[2] = 0.0;
+
+	propobj.pos_ = pos0;
+	propobj.vel_ = vel0;
+	propobj.t_ = 0.0;
+
+	//convert pos and vel into orbital elements
+	propobj.State2OE();
+
+	//set tolerance options
+	propobj.abstol_ = 1*pow(10,-16);
+	propobj.reltol_ = 3*pow(10,-14);
+	propobj.dt_var_ = 0.1;
+
+	//propagate the orbit
+	double dt = 10.0; //TU for propagation
+	const int N = 11; // propagate for 11 steps
+	Eigen::MatrixXd xhist(6,N); //state history
+	Eigen::MatrixXd thist(1,N); //time
+	for (int ii = 0; ii < N; ++ii){
+
+		//propagate
+		propobj.Propagate(dt);
+
+		//update orbital elements
+		propobj.State2OE();
+
+		//store variables
+		xhist.block(0,ii,3,1) = propobj.pos_;
+		xhist.block(3,ii,3,1) = propobj.pos_;
+
+	}
+
+	std::cout << std::setprecision(17);
+	std::cout << "Problem 1: \n";
+	std::cout << "i = 1 position:\n" << xhist.block(0,1,3,1) << std::endl;
+	std::cout << "i = 1 velocity:\n" << xhist.block(3,1,3,1)  << std::endl;
+	std::cout << "i = 10 position:\n" << propobj.pos_ << std::endl;
+	std::cout << "i = 10 velocity:\n" << propobj.vel_ << std::endl;
+
+	//write data to csv
+	Util::Eigen2csv("../data/xhist_HW3.csv",xhist);
+	Util::Eigen2csv("../data/thist_HW3.csv",thist);
+
+	std::cout << "done!" << std::endl;
+	
+	
+} // main
