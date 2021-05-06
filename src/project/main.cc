25222e950f6e1bc632e041c9598e2889fad2739b
coreymarcus <corey.marcus@gmail.com>
2021-05-06 17:25:07 -0500

    debugging drag

--- src/project/main.cc
+++ src/project/main.cc
@@ -31,12 +31,12 @@ int main(int argc, char** argv) {
 	propobj.Rearth_ = 6378.1363; //km
 	propobj.earthrotationspeed_ = 7.292115146706979 * pow(10.0,-5.0); // rad/sec
 	// propobj.C_D_ = 2.0;
-	// propobj.C_D_ = 1.88;
+	propobj.C_D_ = 1.88;
 	// propobj.C_D_ = 1.80;
 	// propobj.C_D_ = 1.70;
 	// propobj.C_D_ = 1.60;
 	// propobj.C_D_ = 1.25;
-	propobj.C_D_ = 0.0;
+	// propobj.C_D_ = 0.0;
 	propobj.A_ = 22; // m^2
 	propobj.m_ = 2000.0; //kg
 	propobj.rho_0_ = 3.614*pow(10.0,-13.0); //kg/m^3
