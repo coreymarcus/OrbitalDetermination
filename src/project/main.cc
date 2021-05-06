2c17f5e35049408ac73f2f5c903cdc0523fc8d70
coreymarcus <corey.marcus@gmail.com>
2021-05-06 17:17:29 -0500

    Update main.cc

--- src/project/main.cc
+++ src/project/main.cc
@@ -35,7 +35,8 @@ int main(int argc, char** argv) {
 	// propobj.C_D_ = 1.80;
 	// propobj.C_D_ = 1.70;
 	// propobj.C_D_ = 1.60;
-	propobj.C_D_ = 1.25;
+	// propobj.C_D_ = 1.25;
+	propobj.C_D_ = 0.0;
 	propobj.A_ = 22; // m^2
 	propobj.m_ = 2000.0; //kg
 	propobj.rho_0_ = 3.614*pow(10.0,-13.0); //kg/m^3
